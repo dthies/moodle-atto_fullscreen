64995183b3e60a3aaed3efdcb69c2bc8c300deac
Daniel Thies <dthies@gmail.com>
2025-04-25 09:59:13 -0500

    Hide course index

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2015122016;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2015122017;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2014051200;        // Requires this Moodle version.
 $plugin->component = 'atto_fullscreen';  // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_STABLE; // Maturity level.
-$plugin->release = '1.0.2';
+$plugin->release = '1.0.3';
